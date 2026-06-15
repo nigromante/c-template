d556615ddfad3794f379a39045bd5b7d4bacb27f
Nigromante <nigromante@github.com>
2026-06-14 20:53:24 -0400

    fix env functions

--- test/run.sh
+++ test/run.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 cwd=$(dirname $(realpath "$0"))
 export LD_LIBRARY_PATH="${cwd}/libs"
-valgrind --leak-check=full --log-file=analisis -s $@ 
+#valgrind --leak-check=full --log-file=analisis -s $@ 
+$@
