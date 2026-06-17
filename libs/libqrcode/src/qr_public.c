c3e20dcbb56930b7dbc2b42d5ed97bd14401d93f
Nigromante <nigromante@github.com>
2026-06-17 15:26:59 -0400

    qrcode parametrize border

--- libs/libqrcode/src/qr_public.c
+++ libs/libqrcode/src/qr_public.c
@@ -8,11 +8,18 @@
 
 #define _PIXEL_(x, y) (qr_data->data[(y) * qr_data->width + (x)] & 1)
 
+#define BORDER 2
+#define BORDER_FULL (2 * BORDER)
+
 #define PIX_SIZE 16
+#define PIX_LEN 3
+#define _PIX_SIZE_ (PIX_SIZE * PIX_LEN)
 
 #define SIZE (size)
 #define WIDTH SIZE
 #define HEIGHT SIZE
+
+#define WIDTH_FULL (BORDER_FULL + WIDTH)
 
 #define BLANCO "\xff\xff\xff"
 #define AMARILLO "\xff\xff\xd0"
@@ -25,33 +32,33 @@
 #define AZUL "\x00\x00\xaf"
 #define NARANJA "\xdf\x7f\x00"
 
-char pix_fg[5][3];
-char pix_bg[5][3];
+char pix_fg[5][PIX_LEN];
+char pix_bg[5][PIX_LEN];
 
 void pix_color_init() {
-    memcpy(pix_fg[0], NEGRO, 3);
-    memcpy(pix_fg[1], ROJO, 3);
-    memcpy(pix_fg[2], VERDE, 3);
-    memcpy(pix_fg[3], AZUL, 3);
-    memcpy(pix_fg[4], NARANJA, 3);
+    memcpy(pix_fg[0], NEGRO, PIX_LEN);
+    memcpy(pix_fg[1], ROJO, PIX_LEN);
+    memcpy(pix_fg[2], VERDE, PIX_LEN);
+    memcpy(pix_fg[3], AZUL, PIX_LEN);
+    memcpy(pix_fg[4], NARANJA, PIX_LEN);
 
-    memcpy(pix_bg[0], BLANCO, 3);
-    memcpy(pix_bg[1], AMARILLO, 3);
-    memcpy(pix_bg[2], AMARILLO2, 3);
-    memcpy(pix_bg[3], NARANJA2, 3);
+    memcpy(pix_bg[0], BLANCO, PIX_LEN);
+    memcpy(pix_bg[1], AMARILLO, PIX_LEN);
+    memcpy(pix_bg[2], AMARILLO2, PIX_LEN);
+    memcpy(pix_bg[3], NARANJA2, PIX_LEN);
 }
 
 void paint_pix(void *buffer) {
     for (int i = 0; i < PIX_SIZE; i++) {
         int r = rand() % 5;
-        memcpy(buffer + i * 3, pix_fg[r], 3);
+        memcpy(buffer + i * PIX_LEN, pix_fg[r], PIX_LEN);
     }
 }
 
 void pix_clear_line(void *buffer, int size) {
-    for (int i = 0; i < size * PIX_SIZE * 3; i += 3) {
+    for (int i = 0; i < size * _PIX_SIZE_; i += PIX_LEN) {
         int r = rand() % 2;
-        memcpy(buffer + i, pix_bg[r], 3);
+        memcpy(buffer + i, pix_bg[r], PIX_LEN);
     }
 }
 
@@ -98,7 +105,7 @@ int qr_2_png(char *text, char *file) {
     png_init_io(png_ptr, fp);
 
     // 5. Write PNG header info (Width, Height, Bit Depth, Color Type)
-    png_set_IHDR(png_ptr, info_ptr, (WIDTH + 2) * PIX_SIZE, (HEIGHT + 2) * PIX_SIZE,
+    png_set_IHDR(png_ptr, info_ptr, WIDTH_FULL * PIX_SIZE, WIDTH_FULL * PIX_SIZE,
                  8,                  // 8 bits per channel
                  PNG_COLOR_TYPE_RGB, // RGB channels (no alpha)
                  PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
@@ -108,30 +115,32 @@ int qr_2_png(char *text, char *file) {
     // 6. Allocate memory for image pixels (3 bytes per pixel for RGB)
 
     // -------------------------------------------------
-    int tt = 3 * WIDTH * PIX_SIZE * sizeof(png_byte) + (2 * PIX_SIZE * 3);
+    int tt = (WIDTH_FULL * _PIX_SIZE_);
+    printf("\n\nwidth %d  /  width_full %d  / total %d ", WIDTH, WIDTH_FULL, tt);
+
     png_bytep row = malloc(tt);
 
-    for (int i = 0; i < PIX_SIZE; i++) {
-        pix_clear_line(row, WIDTH + 2);
+    for (int i = 0; i < PIX_SIZE * BORDER; i++) {
+        pix_clear_line(row, WIDTH + BORDER_FULL);
         png_write_row(png_ptr, row);
     }
 
     for (int y = 0; y < size; y++) {
-        pix_clear_line(row, WIDTH + 2);
+        pix_clear_line(row, WIDTH + BORDER_FULL);
 
         for (int i = 0; i < PIX_SIZE; i++) {
             for (int x = 0; x < size; x++) {
                 int pixel = _PIXEL_(x, y);
                 if (pixel) {
-                    paint_pix(row + 3 * PIX_SIZE + (x * 3 * PIX_SIZE));
+                    paint_pix(row + ((BORDER + x) * _PIX_SIZE_));
                 }
             }
             png_write_row(png_ptr, row);
         }
     }
 
-    for (int i = 0; i < PIX_SIZE; i++) {
-        pix_clear_line(row, WIDTH + 2);
+    for (int i = 0; i < PIX_SIZE * BORDER; i++) {
+        pix_clear_line(row, WIDTH + BORDER_FULL);
         png_write_row(png_ptr, row);
     }
 
